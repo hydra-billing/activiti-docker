b8cd3c7723ed80e39b0e486f36c7cacf7b376314
Nikita Petrov <nixi@kanius.ru>
2017-06-28 18:19:33 +0300

    Add env vars for db port, name, username and password

--- assets/db.properties.pgsql
+++
@@ -1,5 +0,0 @@
-db=postgres
-jdbc.driver=org.postgresql.Driver
-jdbc.url=jdbc:postgresql://{{DB_HOST}}:5432/activiti
-jdbc.username=activiti
-jdbc.password=activiti
--- jdbc.sh
+++ jdbc.sh
@@ -3,35 +3,43 @@ set -x
 
 DB_TYPE=$(echo $DB_TYPE | tr '[:upper:]' '[:lower:]')
 
-# check for linked database containers
-if [[ -n $POSTGRESQL_PORT_5432_TCP_ADDR ]]; then
-	DB_HOST="$POSTGRESQL_PORT_5432_TCP_ADDR"
-elif [[ -n $MYSQL_PORT_3306_TCP_ADDR ]]; then
-	DB_HOST="$MYSQL_PORT_3306_TCP_ADDR"
-else
-	DB_HOST="$DB_HOST"
-fi
 
-sed -i -e "s/{{DB_HOST}}/$DB_HOST/" /assets/db.properties.pgsql
-sed -i -e "s/{{DB_HOST}}/$DB_HOST/" /assets/db.properties.mysql
+cd /opt
 
-case $DB_TYPE in
-	postgres )	cp /assets/db.properties.pgsql $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/classes/db.properties \
-			&& cp /assets/db.properties.pgsql $CATALINA_HOME/webapps/activiti-rest/WEB-INF/classes/db.properties
-			cd /opt
+case "$DB_TYPE" in
+	postgres)
+      if [[ -n "$POSTGRESQL_PORT_5432_TCP" ]]; then
+        DB_HOST="$POSTGRESQL_PORT_5432_TCP_ADDR"
+        DB_PORT="$POSTGRESQL_PORT_5432_TCP_PORT"
+      fi
+
 			wget https://jdbc.postgresql.org/download/postgresql-${PGSQL_JDBC_VERSION}.jar
-			cp postgresql-${PGSQL_JDBC_VERSION}.jar $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/lib/ \
-			&& cp postgresql-${PGSQL_JDBC_VERSION}.jar $CATALINA_HOME/webapps/activiti-rest/WEB-INF/lib/
+			cp postgresql-${PGSQL_JDBC_VERSION}.jar $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/lib/
+			cp postgresql-${PGSQL_JDBC_VERSION}.jar $CATALINA_HOME/webapps/activiti-rest/WEB-INF/lib/
 			;;
-	mysql )		cp /assets/db.properties.mysql $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/classes/db.properties \
-			&& cp /assets/db.properties.mysql $CATALINA_HOME/webapps/activiti-rest/WEB-INF/classes/db.properties
-			cd /opt
+	mysql)
+      if [[ -n "$MYSQL_PORT_3306_TCP" ]]; then
+      	DB_HOST="$MYSQL_PORT_3306_TCP_ADDR"
+      	DB_PORT="$MYSQL_PORT_3306_TCP_PORT"
+      fi
+      
 			wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-${MYSQL_JDBC_VERSION}.tar.gz
 			tar -xf mysql-connector-java-${MYSQL_JDBC_VERSION}.tar.gz
-			cp mysql-connector-java-${MYSQL_JDBC_VERSION}/mysql-connector-java-${MYSQL_JDBC_VERSION}-bin.jar $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/lib/ \
-			&& cp mysql-connector-java-${MYSQL_JDBC_VERSION}/mysql-connector-java-${MYSQL_JDBC_VERSION}-bin.jar $CATALINA_HOME/webapps/activiti-rest/WEB-INF/lib/
+			cp mysql-connector-java-${MYSQL_JDBC_VERSION}/mysql-connector-java-${MYSQL_JDBC_VERSION}-bin.jar $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/lib/
+			cp mysql-connector-java-${MYSQL_JDBC_VERSION}/mysql-connector-java-${MYSQL_JDBC_VERSION}-bin.jar $CATALINA_HOME/webapps/activiti-rest/WEB-INF/lib/
 			;;
-	* )		echo "Unknown database type"
+	*)
+      echo "Unknown database type"
 			exit 1
 			;;
 esac
+
+
+sed -i -e "s/{{DB_HOST}}/$DB_HOST/" /assets/db.properties.$DB_TYPE
+sed -i -e "s/{{DB_PORT}}/$DB_PORT/" /assets/db.properties.$DB_TYPE
+sed -i -e "s/{{DB_NAME}}/$DB_NAME/" /assets/db.properties.$DB_TYPE
+sed -i -e "s/{{DB_USERNAME}}/$DB_USERNAME/" /assets/db.properties.$DB_TYPE
+sed -i -e "s/{{DB_PASSWORD}}/$DB_PASSWORD/" /assets/db.properties.$DB_TYPE
+
+cp /assets/db.properties.$DB_TYPE $CATALINA_HOME/webapps/activiti-explorer/WEB-INF/classes/db.properties
+cp /assets/db.properties.$DB_TYPE $CATALINA_HOME/webapps/activiti-rest/WEB-INF/classes/db.properties
